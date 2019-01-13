6dc39ba3ae445e6097593ecdad28fe068f8a5159
sanketfirodiya <sanket.firodiya@gmail.com>
2019-01-12 19:50:04 -0800

    first commit

---
+++ Pods/Target Support Files/Pods-RageTweet/Pods-RageTweet-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RageTweet : NSObject
+@end
+@implementation PodsDummy_Pods_RageTweet
+@end
---
+++ RageTweet/ViewController.swift
@@ -0,0 +1,109 @@
+/// Copyright (c) 2019 Razeware LLC
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+///
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+
+import UIKit
+import TwitterKit
+
+class ViewController: UIViewController, UIScrollViewDelegate {
+  @IBOutlet weak var skyView: SkyView!
+  @IBOutlet weak var containerScrollView: ContainerScrollView!
+
+  private let tweets: [String] = ["The world is my oyster - raw, wriggling, and danglng from my mouth.",
+                                  "Get down with your bad self!",
+                                  "Hmm ... Not good, not bad.  Not happy.  Not sad.",
+                                  "Core breach imminent - stand back before I go thermonuclear",
+                                  "FFFFFFFUUUUUUUUU!!!!1!!!"]
+
+  private let ragePhotos: [UIImage?] = [UIImage.init(named: "happy.png"),
+                                        UIImage.init(named: "content.png"),
+                                        UIImage.init(named: "no-amused.png"),
+                                        UIImage.init(named: "irked.png"),
+                                        UIImage.init(named: "raging.png")]
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    let viewWidth = view.frame.size.width
+    let scrollView = UIScrollView(frame: CGRect(x: viewWidth/4, y: 0, width: viewWidth/2, height: 300))
+    scrollView.contentSize = CGSize(width: viewWidth * 2.5, height: 200)
+    scrollView.isPagingEnabled = true
+    scrollView.clipsToBounds = false
+    scrollView.delegate = self;
+
+    let scrollViewWidth = scrollView.frame.size.width
+    for (index, photo) in ragePhotos.enumerated() {
+      let imageView = UIImageView.init(image: photo)
+      let currentXOffset: CGFloat = (scrollViewWidth/2 - imageView.frame.size.width/2) + CGFloat(index) * scrollViewWidth
+      let button = UIButton(frame: CGRect(x: currentXOffset, y: 0, width: imageView.frame.size.width, height: imageView.frame.size.height))
+      button.tag = index
+      button.setImage(photo, for: .normal)
+      button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+      scrollView.addSubview(button)
+    }
+
+    containerScrollView.setScrollView(scrollView)
+  }
+
+  func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+    let pageNum: Int = (Int(scrollView.contentOffset.x / scrollView.frame.size.width));
+    if let rageLevel = RageLevel(rawValue: pageNum) {
+      skyView.setRageLevel(rageLevel)
+    }
+  }
+
+  @objc func buttonTapped(sender: UIButton) {
+    if (TWTRTwitter.sharedInstance().sessionStore.hasLoggedInUsers()) {
+      showTweetCompose(sender.tag)
+    } else {
+      TWTRTwitter.sharedInstance().logIn { [weak self] session, error in
+        guard let self = self else { return }
+
+        if session != nil {
+          self.showTweetCompose(sender.tag)
+        } else {
+          let alert = UIAlertController(title: "Twitter Unavailable", message: "Please turn to your nearest mirror and rage away", preferredStyle: .alert)
+          alert.addAction(UIAlertAction(title: "Okay", style: .default, handler: nil))
+          self.present(alert, animated: false, completion: nil)
+        }
+      }
+    }
+  }
+
+  private func showTweetCompose(_ index: Int) {
+    let composer = TWTRComposer()
+
+    if tweets.count > index {
+      composer.setText(tweets[index])
+    }
+
+    if ragePhotos.count > index {
+      composer.setImage(ragePhotos[index])
+    }
+
+    composer.show(from: self, completion: nil)
+  }
+}
